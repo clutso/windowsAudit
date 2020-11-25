8781c55d62b0d8fd04f39afffcdc6e6e2fb272ad
clutso <clutso@hotmail.com>
2020-11-25 08:43:51 -0600

    file main.bat created -- first ver --

---
+++ main.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ----------------------------------
+echo Hello I wil try to list your files
+echo ----------------------------------
+dir
+echo ----------------------------------
+echo Done! have a nice day
+echo ----------------------------------
